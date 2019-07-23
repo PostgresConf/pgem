87fccfecedcdc79177cdb2f5a49c5d7b6692c77d
Eugene Dubinin <eugend@commandprompt.com>
2019-07-23 15:46:29 +0300

    fixes nil exception in scheduled_event_distribution

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -381,8 +381,8 @@ class Conference < ActiveRecord::Base
   # * +hash+ -> hash
   def scheduled_event_distribution
     confirmed_events = program.events.where(state: 'confirmed')
-    scheduled_count = program.selected_event_schedules.count
-    scheduled_value =  { 'value' => program.selected_event_schedules.count, 'color' => 'green' }
+    scheduled_count = program.selected_event_schedules.try(:count) || 0
+    scheduled_value =  { 'value' => scheduled_count, 'color' => 'green' }
     unscheduled_value =  { 'value' => confirmed_events.count - scheduled_count, 'color' => 'red' }
     { 'Scheduled' => scheduled_value, 'Unscheduled' => unscheduled_value }
   end
