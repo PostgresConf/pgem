bc3b97cfc6a63c8af13e0ae09415c7e428afa73a
Eugene Dubinin <eugend@commandprompt.com>
2019-08-23 12:59:21 +0300

    fixes tinymce past plugin loading in production

---
+++ app/assets/javascripts/refinery/tinymce_manifest.js
@@ -0,0 +1,20 @@
+/*
+ * TINYMCE MZFK
+ */
+
+/*
+   Namespace: TINYMCE
+   Global Tinymce namespace.
+*/
+/*
+ *= require tinymce/tinymce
+ *= require tinymce/tinymce.jquery
+ *= require tinymce/plugins/link/plugin
+ *= require tinymce/plugins/image/plugin
+ *= require tinymce/plugins/fullscreen/plugin
+ *= require tinymce/plugins/stylebuttons/plugin
+ *= require tinymce/plugins/code/plugin
+ *= require tinymce/plugins/paste/plugin
+ *= require refinery/boot_tinymce
+ *= require_self
+*/
