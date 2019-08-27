16c9a6b88b06af63a0c17c21156daeba07c1bac8
Eugene Dubinin <eugend@commandprompt.com>
2019-08-27 15:51:27 +0300

    dont show ticket price and total columns if ticket has zero price

--- app/assets/javascripts/osem-tickets.js
+++ app/assets/javascripts/osem-tickets.js
@@ -68,7 +68,7 @@ function restore_state () {
 }
 
 $( document ).ready(function() {
-    if (typeof tix !== 'undefined') {
+    if (tix) {
       document.getElementById('tickets__' + tix).value = '1';
     }
 
