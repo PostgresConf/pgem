3104d93f3ccc3922ce75ea17c36c5c7238f46915
Eugene Dubinin <eugend@commandprompt.com>
2019-08-28 19:51:50 +0300

    fix js error

--- app/assets/javascripts/osem-tickets.js
+++ app/assets/javascripts/osem-tickets.js
@@ -68,7 +68,7 @@ function restore_state () {
 }
 
 $( document ).ready(function() {
-    if (tix) {
+    if (typeof(tix) !== 'undefined' && tix) {
       document.getElementById('tickets__' + tix).value = '1';
     }
 
