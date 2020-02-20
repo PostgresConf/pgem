d3a2b5f8249deda50c0b3229ce6f6aea748a88fa
Eugene Dubinin <eugend@commandprompt.com>
2020-02-20 15:46:16 +0200

    fix boomset integration error

--- app/models/boomset_guest.rb
+++ app/models/boomset_guest.rb
@@ -11,53 +11,54 @@ class BoomsetGuest < ActiveRecord::Base
   end
 
   def self.get_guest(registration)
-      pt = PhysicalTicket.where(registration_id: registration.id)
-                         .joins(:ticket_purchase)
-                         .joins(:ticket)
-                         .order("tickets.position").first
+    pt = PhysicalTicket.where(registration_id: registration.id)
+                        .joins(:ticket_purchase)
+                        .joins(:ticket)
+                        .order("tickets.position").first
+    if pt.nil?
+    ##
+    # There are no tickets so this registration is no longer valid
+    ##
+	    g = nil
+    else
+      g = BoomsetAttendee.new
 
-      if pt.nil?
-        ##
-	# There are no tickets so this registration is no longer valid
-	##
-	g = nil
-      else
-        g = BoomsetAttendee.new
+      unless registration.first_name.blank?
+          g.first_name = registration.first_name
+      end
 
-        unless registration.first_name.blank?
-            g.first_name = registration.first_name
-        end
+      unless registration.last_name.blank?
+          g.last_name = registration.last_name
+      end
 
-        unless registration.last_name.blank?
-            g.last_name = registration.last_name
-        end
+      unless registration.email.blank?
+          g.email = registration.email
+      end
 
-        unless registration.email.blank?
-            g.email = registration.email
-        end
+      unless registration.mobile.blank?
+          g.cell_phone = registration.mobile
+      end
 
-        unless registration.mobile.blank?
-            g.cell_phone = registration.mobile
-        end
+      unless registration.affiliation.blank?
+          g.company = registration.affiliation
+      end
 
-        unless registration.affiliation.blank?
-            g.company = registration.affiliation
-        end
+      unless registration.title.blank?
+          g.job_title = registration.title
+      end
 
-        unless registration.title.blank?
-            g.job_title = registration.title
-        end
+      unless pt.token.blank?
+        g.barcode = pt.token
+        g.nfc_tag = pt.token
+      end
 
-        unless pt.token.blank?
-            g.barcode = pt.token
-	    g.nfc_tag = pt.token
-        end
-
-        btc = BoomsetTicketConfig.where(conference_id: registration.conference_id, ticket_id: pt.ticket.id).first
-
-        unless btc.boomset_registration_type.blank?
-	    g.registration_type = btc.boomset_registration_type
-        end
+      btc = BoomsetTicketConfig.where(conference_id: registration.conference_id, ticket_id: pt.ticket.id).first
+      if btc
+          unless btc.boomset_registration_type.blank?
+          g.registration_type = btc.boomset_registration_type
+          end
+      end
+      
       end
 
       g
