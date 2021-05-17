b65926b03aad77409551d2b1bee320a49642cab2
Eugene Dubinin <eugend@commandprompt.com>
2021-05-17 13:27:29 +0300

    makes ahoy models to user relations optional

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -2,5 +2,5 @@ class Ahoy::Visit < ActiveRecord::Base
   self.table_name = "ahoy_visits"
 
   has_many :ahoy_events, class_name: "Ahoy::Event"
-  belongs_to :user
+  belongs_to :user, optional: true
 end
