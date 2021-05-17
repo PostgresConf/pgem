b72f567c0a1421d6ab8a4cf1975e632d0e46e73c
Eugene Dubinin <eugend@commandprompt.com>
2021-05-17 13:43:06 +0300

    updates before_filter to before_action in controller

--- app/controllers/ticket_purchases_controller.rb
+++ app/controllers/ticket_purchases_controller.rb
@@ -10,11 +10,6 @@ class TicketPurchasesController < ApplicationController
     code_id = params[:code_id] || ''
     chosen_events = params[:chosen_events] || []
 
-    unless verify_recaptcha
-      return redirect_to conference_tickets_path(@conference.short_title)
-    end
-
-
     if tkts[0].blank?
       return redirect_to conference_tickets_path(@conference.short_title),
       error: 'Please get at least one ticket to continue.'
