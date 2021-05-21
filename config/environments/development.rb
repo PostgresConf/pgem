6e549a1d2888c87507ace4ff663ad0ac8539e403
Eugene Dubinin <eugend@commandprompt.com>
2021-05-21 18:48:20 +0300

    use in-memory view cache for development; removes locale from default url options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Osem::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  
+
   # Use letter_opener_web for Vagrant (launchy won't work)
   config.action_mailer.delivery_method = ENV['USER'] == 'vagrant' ? :letter_opener_web : :letter_opener
 
@@ -34,11 +34,15 @@ Osem::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.cache_store = :memory_store, { size: 64.megabytes }
+
   # Set the detault url for action mailer
   config.action_mailer.default_url_options = { host: (ENV['OSEM_HOSTNAME'] || 'localhost:3000') }
 
   # Set the secret key base if it's not set via other means
   config.secret_key_base ||= 'f4be765bc98e516de82ac01daa8f8aa11c5ca13cb6c911887851ac89457b6c0b056b2361a21b5c08926c9386e0f91eef84fc0b103d522bf00bc0c78ea8ce7c58'
+
+  config.action_controller.default_url_options = { locale: nil }
 
   # Use omniauth mock credentials
   OmniAuth.config.test_mode = true
