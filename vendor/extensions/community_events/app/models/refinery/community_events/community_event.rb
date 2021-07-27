e83c6f4511f95161dfebd23d1180265cf12e30c9
Eugene Dubinin <eugend@commandprompt.com>
2021-07-27 13:30:30 +0300

    fixes search for meetups and community events

--- vendor/extensions/community_events/app/models/refinery/community_events/community_event.rb
+++ vendor/extensions/community_events/app/models/refinery/community_events/community_event.rb
@@ -8,7 +8,7 @@ module Refinery
 
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
-      #   acts_as_indexed :fields => [:title]
+      acts_as_indexed :fields => [:title]
       class << self
         def newest_first
           order("published_at DESC")
--- vendor/extensions/meetups/app/models/refinery/meetups/meetup.rb
+++ vendor/extensions/meetups/app/models/refinery/meetups/meetup.rb
@@ -16,7 +16,7 @@ module Refinery
 
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
-      #   acts_as_indexed :fields => [:title]
+        acts_as_indexed :fields => [:title]
 
     end
   end
