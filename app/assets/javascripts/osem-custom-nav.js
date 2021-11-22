bd7aba7ae697cf9eb00903c491e7556b47008420
Eugene Dubinin <eugend@commandprompt.com>
2021-11-22 16:48:26 +0200

    extract navbar js into separate file

---
+++ app/assets/javascripts/osem-custom-nav.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    if($('.navbar-custom').length) {
+      $(window).on('scroll load', function(){
+          let top = $(window).scrollTop();
+
+          // skip if user clicked navbar-toggle and the menu is shown
+          if($('.navbar-custom.expanded').length === 0) {
+            if (top > 50) {
+                $('#site-nav').addClass('navbar-solid');
+            }
+            else {
+                $('#site-nav').removeClass('navbar-solid');
+            }
+          }
+      })
+
+      $('.navbar-toggle').click(function(){
+        var top = $(window).scrollTop();
+        if(top <= 50) {
+          $('#site-nav').toggleClass('navbar-solid expanded');
+        }
+      });
+    }
+})
