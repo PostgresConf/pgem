546038b078ee8f66a9c0901a25a985d316db12d8
Eugene Dubinin <eugend@commandprompt.com>
2022-04-08 15:58:14 +0300

    implements server site processing for users admin UI datatables

--- app/assets/javascripts/osem-datatables.js
+++ app/assets/javascripts/osem-datatables.js
@@ -1,38 +1,62 @@
 $(function () {
   $(document).ready(function() {
+
+    function switchChangeHandler(event, state) {
+      let url = $(this).attr('url') + state;
+      let method = $(this).attr('method');
+      $.ajax({
+        url: url,
+        type: method,
+        dataType: 'script'
+      });
+    }
+
     $('.datatable').DataTable({
-      // ajax: ...,
       stateSave: true,
       autoWidth: false,
       pagingType: 'full_numbers',
-      "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]]
+      'lengthMenu': [[25, 50, 100, -1], [25, 50, 100, "All"]]
     });
 
     $('.datatable-events').DataTable({
-      // ajax: ...,
       stateSave: true,
       autoWidth: false,
       pagingType: 'full_numbers',
-      "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+      'lengthMenu': [[25, 50, 100, -1], [25, 50, 100, "All"]],
       // initialize bs switch on each page draw,
-      "fnDrawCallback": function() {
+      'lengthMenu': function() {
         $("[class='event-switch-checkbox']").bootstrapSwitch();
-
-        $('input[class="event-switch-checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
-          var url = $(this).attr('url') + state;
-          var method = $(this).attr('method');
-
-          $.ajax({
-            url: url,
-            type: method,
-            dataType: 'script'
-          });
-        });
+        $("input[class='event-switch-checkbox']").on('switchChange.bootstrapSwitch', switchChangeHandler);
       }
+    });
+
+    $('.datatable-users').DataTable({
+      ajax: $('.datatable-users').data('source'),
+      serverSide: true,
+      stateSave: true,
+      autoWidth: true,
+      processing: true,
+      pagingType: 'full_numbers',
+      "lengthMenu": [[25, 50, 100, 250], [25, 50, 100, 250]],
+      "fnDrawCallback": function() {
+        $("[class='switch-checkbox']").bootstrapSwitch();
+
+        $('input[class="switch-checkbox"]').on('switchChange.bootstrapSwitch', switchChangeHandler);
+      },
+      columns: [
+        {data: 'id'},
+        {data: 'confirmed', 'searchable': false, 'orderable': false },
+        {data: 'email'},
+        {data: 'name'},
+        {data: 'roles'},
+        {data: 'dt_action_view', 'searchable': false, 'orderable': false },
+        {data: 'dt_action_edit', 'searchable': false, 'orderable': false },
+      ]
     });
 
     $('#versionstable').DataTable({
       pagingType: 'full_numbers',
+      autoWidth: true,
       order: [[ 0, 'desc' ]]
     });
   });
