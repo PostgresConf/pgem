f857b528ba987b87bbd32d62472d3fe2b2ff2bc6
Eugene Dubinin <eugend@commandprompt.com>
2022-05-24 16:34:21 +0300

    fixes permission check in poll results report

--- app/controllers/admin/reports/poll_results_controller.rb
+++ app/controllers/admin/reports/poll_results_controller.rb
@@ -3,7 +3,6 @@ module Admin
     class PollResultsController < Admin::BaseController
       load_and_authorize_resource :conference, find_by: :short_title
       load_and_authorize_resource :poll, through: :conference, singleton: true
-      load_and_authorize_resource :survey, through: :poll, singleton: true
 
       def index
         @poll_results = PollResult.joins(:survey_question)
