edcc6fc81f6769197fcf2c57645c51320018b68e
Eugene Dubinin <eugend@commandprompt.com>
2022-05-27 15:45:09 +0300

    fixes deprecated redirect_to :back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -195,7 +195,7 @@ module ApplicationHelper
   # Same as redirect_to(:back) if there is a valid HTTP referer, otherwise redirect_to()
   def redirect_back_or_to(options = {}, response_status = {})
     if request.env['HTTP_REFERER']
-      redirect_to :back
+      redirect_back fallback_location:'/'
     else
       redirect_to options, response_status
     end
