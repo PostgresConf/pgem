952649673175378f337737c03cc2ef2e5e35ede5
Eugene Dubinin <eugend@commandprompt.com>
2024-10-25 13:49:25 +0300

    show promocode name in "free ticket" purchase message refs: #194274

--- app/controllers/ticket_purchases_controller.rb
+++ app/controllers/ticket_purchases_controller.rb
@@ -37,9 +37,19 @@ class TicketPurchasesController < ApplicationController
       elsif current_user.ticket_purchases.by_conference(@conference).paid.any?
         # Trigger ahoy event
         ahoy.track 'Ticket purchase', title: 'New free purchase'
+        # unpaid ticket purchases were destroyed above; if we have paid purchases at this moment
+        # this means that .purchase method parked purchase as paid automatically
+        # this happens on zero payment amount only hence "free ticket message"
+        last_purchase = current_user.ticket_purchases.by_conference(@conference).paid.last
+
+        if last_purchase.code.present?
+          notice = "You have free tickets for the conference obtained via #{last_purchase.code.name}"
+        else
+          notice = 'You have free tickets for the conference.'
+        end
 
         redirect_to complete_conference_tickets_path,
-                    notice: 'You have free tickets for the conference.'
+                    notice: notice
       else
         redirect_to conference_tickets_path(@conference.short_title),
                     error: 'Please get at least one ticket to continue.'
