071c23159dc091f7aad41fcdf12af1450d55fbd3
Eugene Dubinin <eugend@commandprompt.com>
2024-12-04 16:19:39 +0200

    don't try lookup up for omniauth stuff, prevents app from booting in prod

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,9 +3,9 @@ module Users
     skip_before_filter :verify_authenticity_token
     skip_authorization_check
 
-    User.omniauth_providers.each do |provider|
-      define_method(provider) { handle(provider) }
-    end
+    #User.omniauth_providers.each do |provider|
+    #  define_method(provider) { handle(provider) }
+    #end
 
     private
 
